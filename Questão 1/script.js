ff419f6e45446216e4c25ade396e5e824ba03919
guicms03 <144068360+guicms03@users.noreply.github.com>
2024-04-15 23:47:53 +0000

    ativiade9

--- Questão 1/script.js
+++ Questão 1/script.js
@@ -1,10 +1,37 @@
-function somaAteN(N) {
-    if (N === 1) {
-        return 1;
-    } else {
-        return N + somaAteN(N - 1);
+class Aluno {
+    constructor(id, nome, nota) {
+        this.id = id;
+        this.nome = nome;
+        this.nota = nota;
     }
 }
 
-let resultado = somaAteN(5);
-console.log("A soma dos números de 1 até N é:", resultado);
+function adicionarAluno(listaAlunos, id, nome, nota) {
+    const aluno = new Aluno(id, nome, nota);
+    listaAlunos.push(aluno);
+}
+
+function buscarAlunoPorId(listaAlunos, id) {
+    return listaAlunos.find(aluno => aluno.id === id) || null;
+}
+
+function calcularMediaNotas(listaAlunos) {
+    const quantidadeAlunos = listaAlunos.length;
+    if (quantidadeAlunos === 0) {
+        return 0;
+    }
+
+    const totalNotas = listaAlunos.reduce((total, aluno) => total + aluno.nota, 0);
+    return totalNotas / quantidadeAlunos;
+}
+
+// Exemplo de uso:
+const alunos = [];
+
+adicionarAluno(alunos, 1, "João", 8.5);
+adicionarAluno(alunos, 2, "Maria", 7.9);
+adicionarAluno(alunos, 3, "Pedro", 6.0);
+
+const media = calcularMediaNotas(alunos);
+console.log("Média das notas dos alunos:", media);
+
--- Questão 2/script.js
+++ Questão 2/script.js
@@ -1,10 +1,31 @@
-function fatorial(N) {
-    if (N === 1) {
-        return 1;
-    } else {
-        return N * fatorial(N - 1);
-    }
+function adicionarAluno(dicionarioAlunos, id, nome, nota) {
+    dicionarioAlunos[id] = { nome: nome, nota: nota };
 }
 
-let resultadoFatorial = fatorial(5);
-console.log("O fatorial de 5 é:", resultadoFatorial);
+function buscarAlunoPorId(dicionarioAlunos, id) {
+    return dicionarioAlunos[id] || null;
+}
+
+function calcularMediaNotas(dicionarioAlunos) {
+    const ids = Object.keys(dicionarioAlunos);
+    if (ids.length === 0) {
+        return 0;
+    }
+
+    let totalNotas = 0;
+    ids.forEach(id => {
+        totalNotas += dicionarioAlunos[id].nota;
+    });
+
+    return totalNotas / ids.length;
+}
+
+// Exemplo de uso:
+const alunos = {};
+
+adicionarAluno(alunos, 1, "João", 8.5);
+adicionarAluno(alunos, 2, "Maria", 7.9);
+adicionarAluno(alunos, 3, "Pedro", 6.0);
+
+const media = calcularMediaNotas(alunos);
+console.log("Média das notas dos alunos:", media);
